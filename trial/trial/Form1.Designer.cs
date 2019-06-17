d6b3b01d9fc90e2f910a5a74208d6356defa9213
jacobcapes <51932939+jacobcapes@users.noreply.github.com>
2019-06-18 10:41:39 +1200

    Project Files uploaded

---
+++ trial/trial/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace trial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PanelGame = new System.Windows.Forms.Panel();
+            this.TmrCar = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PanelGame
+            // 
+            this.PanelGame.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.PanelGame.Location = new System.Drawing.Point(12, 28);
+            this.PanelGame.Name = "PanelGame";
+            this.PanelGame.Size = new System.Drawing.Size(682, 410);
+            this.PanelGame.TabIndex = 0;
+            this.PanelGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelGame_Paint);
+            // 
+            // TmrCar
+            // 
+            this.TmrCar.Enabled = true;
+            this.TmrCar.Interval = 50;
+            this.TmrCar.Tick += new System.EventHandler(this.TmrCar_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PanelGame);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelGame;
+        private System.Windows.Forms.Timer TmrCar;
+    }
+}
+
---
+++ trial/trial/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trial
+{
+    public partial class Form1 : Form
+    {
+        Graphics g; // declare the graphics object
+        int x = 20, y = 20;// starting position of planet
+        Rectangle area;
+        Image beetle = Image.FromFile(Application.StartupPath + @"\beetle.jpg");
+        Image beetleright = Image.FromFile(Application.StartupPath + @"\beetle right.jpg");
+        Image beetleleft = Image.FromFile(Application.StartupPath + @"\beetle left.jpg");
+        Image beetledown = Image.FromFile(Application.StartupPath + @"\beetle down.jpg");
+
+
+        bool left, right, up, down;
+
+        private void TmrCar_Tick(object sender, EventArgs e)
+        {
+            if (left) { area.X -= 5; }//move ship 5 to the left
+            if (right) { area.X += 5; }// move ship 5 to the right
+            if (up) { area.Y -= 5; }//move ship 5 to the up
+            if (down) { area.Y += 5; }// move ship 5 to the down
+            PanelGame.Invalidate();
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = false;}
+            if (e.KeyData == Keys.Right) { right = false; }
+            if (e.KeyData == Keys.Up) { up = false; }
+            if (e.KeyData == Keys.Down) { down = false; }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left)  { left = true; }
+            if (e.KeyData == Keys.Right) { right = true; }
+            if (e.KeyData == Keys.Up) { up = true; }
+            if (e.KeyData == Keys.Down) { down = true; }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            area = new Rectangle(x, y, 30, 30);//spaceship's rectangle	
+        }
+
+        private void PanelGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the methods from the graphic's class to paint on the panel
+            g = e.Graphics;
+            //use the DrawImage method to draw the spaceship on the panel
+            g.DrawImage(beetle, area);
+            e.Graphics.RotateTransform(20.0F);
+            e.Graphics.DrawImage(beetle, new Rectangle(5, 5, 30, 40));
+        }
+
+    }
+}
